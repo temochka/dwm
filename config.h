37d0725f531e39004b4f582b1b130b760aed6e15
Artem Chistyakov <chistyakov.artem@gmail.com>
2015-10-04 13:09:41 -0400

    Adjust dmenufont (although not used).

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const char *fonts[] = {
     "VL Gothic:size=10.5",
     "WenQuanYi Micro Hei:size=10.5",
 };
-static const char dmenufont[] = "-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
+static const char dmenufont[] = "-*-terminus-medium-r-*-*-10-*-*-*-*-*-*-*";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
