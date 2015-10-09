69933828acac23e8065fc1619c0c2d4fc641ea68
Artem Chistyakov <chistyakov.artem@gmail.com>
2015-10-08 21:06:39 -0400

    Use unicode for layout icons.

--- config.h
+++ config.h
@@ -38,9 +38,9 @@ static const Bool resizehints = False; /* True means respect size hints in tiled
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "◫",      tile },    /* first entry is default */
+	{ "◰",      NULL },    /* no layout function means floating behavior */
+	{ "◘",      monocle },
 };
 
 /* key definitions */
