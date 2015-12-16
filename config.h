b956da634563daac1c064d6599bbfc810ea6fb6a
Artem Chistyakov <chistyakov.artem@gmail.com>
2015-12-16 16:07:27 -0500

    Disable Cmd+Tab shortcut.

--- config.h
+++ config.h
@@ -73,7 +73,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_equal,  setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+//	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_w,      killclient,     {0} },
 	{ MODKEY|Mod1Mask,              XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|Mod1Mask,              XK_f,      setlayout,      {.v = &layouts[1]} },
